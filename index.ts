963d0750d25d5c6c5fc5e3074ba335f05640d909
Robin Guinant <robin@guinant.com>
2022-06-07 18:02:00 +0200

    Update index.ts
    Remove trash code

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { getErp, getErps } from "./methods";
-
 export function hello(firstName: string): string {
   return `Hello ${firstName}!`;
 }
@@ -8,13 +6,5 @@ export * from "./types";
 export * from "./methods";
 
 (async () => {
-  //   const erps = await getErps({ page: 1, q: "montargis" });
-  //   console.log(`TOTAL: ${erps.count}`);
-  //   const x: any = [];
-  //   for (const erp of erps.results) {
-  //     x.push({ name: erp.nom, slug: erp.slug });
-  //   }
-  //   console.table(x);
-  //   const erp = await getErp("mairie-montargis");
-  //   console.table(erp);
+  // debug
 })();
