2148c218b13b1219c0b4dde4313c8e7fe4bd6332
Robin Guinant <robin@guinant.com>
2022-06-08 12:27:14 +0200

    Add utils/accessibilite
    - isValidAccessibiliteApiUrl
    - extractAccessibiliteSlugFromApiUrl

--- index.ts
+++ index.ts
@@ -1,10 +1,2 @@
-export function hello(firstName: string): string {
-  return `Hello ${firstName}!`;
-}
-
 export * from "./types";
 export * from "./methods";
-
-(async () => {
-  // debug
-})();
--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,4 @@ export * from "./constants";
 export * from "./paginate";
 export * from "./url";
 export * from "./erp";
+export * from "./accessibilite";
